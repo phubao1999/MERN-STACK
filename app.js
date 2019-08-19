a7255cf782e6da0e8d3f30c993d903fe3f3de2fb
phubao1999 <phutuongbao1999@gmail.com>
2019-08-19 23:25:10 +0700

    Update Search Form And Nodemon Module

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8080
+
 app.set('view engine', 'pug')
 app.set('views', './views')
+
+let users = [
+    { id: 1, name: 'Bao' },
+    { id: 2, name: 'Eden' },
+    { id: 3, name: 'Frank' },
+    { id: 4, name: 'Wilian' },
+    { id: 5, name: 'Cech' },
+]
 
 app.get('/', (req, res) => res.render('index', {
     name: 'Nodejs'
 }));
 app.get('/users', (req, res) => res.render('users/index', {
-    users: [
-        { id: 1, name: 'Bao' },
-        { id: 2, name: 'Eden' },
-        { id: 3, name: 'Frank' },
-        { id: 4, name: 'Wilian' },
-        { id: 5, name: 'Cech' },
-    ]
+    users: users
 }))
+
+app.get('/users/search', (req, res) => {
+    var q = req.query.q;
+    var matchedUsers = users.filter(user => {
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    })
+    res.render('users/index', {
+        users: matchedUsers
+    });
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

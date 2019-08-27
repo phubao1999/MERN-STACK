b91dfc36f0cf9e1a042cfdcc734bcb8457c804e5
phubao1999 <phutuongbao1999@gmail.com>
2019-08-27 23:03:29 +0700

    Update LowDB

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 8080
 var bodyParser = require('body-parser');
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+db.defaults({ users: [] })
+  .write()
 
 app.set('view engine', 'pug')
 app.set('views', './views')
@@ -9,23 +16,15 @@ app.set('views', './views')
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-let users = [
-    { id: 1, name: 'Bao' },
-    { id: 2, name: 'Eden' },
-    { id: 3, name: 'Frank' },
-    { id: 4, name: 'Wilian' },
-    { id: 5, name: 'Cech' },
-]
-
 app.get('/', (req, res) => res.render('index', {
     name: 'Nodejs'
 }));
 app.get('/users', (req, res) => res.render('users/index', {
-    users: users
+    users: db.get('users').value()
 }))
 app.get('/users/create', (req, res) => res.render('users/create'));
 app.post('/users/create', (req, res) => {
-    users.push(req.body);
+    db.get('users').push(req.body).write();
     res.redirect('/users');
 })
 
